b6460717c693855ed47fc5ca0d4cebf84a67c9b3
shogo314 <80336945+shogo314@users.noreply.github.com>
2025-06-07 01:17:59 +0900

    update

--- tools/download.py
+++ tools/download.py
@@ -1,5 +1,19 @@
+import argparse
 import download.hoj as hoj
+import download.kep as kep
 
-# 例: 1ページ目を取得
+
+def main():
+    parser = argparse.ArgumentParser(description="Download from competitive programming sites.")
+    parser.add_argument("site", choices=["hoj", "kep"], help="Choose the site to download from.")
+
+    args = parser.parse_args()
+
+    if args.site == "hoj":
+        hoj.download()
+    elif args.site == "kep":
+        kep.download()
+
+
 if __name__ == "__main__":
-    hoj.download()
+    main()
